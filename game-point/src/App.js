d61c1c5772b8763fa68d0ea5e7250ef1f65b8e53
Victor Hugo Castilho <victorhugocastilhomachado@gmail.com>
2023-10-04 14:17:23 -0300

    menu inicial

---
+++ game-point/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import 'bootswatch/dist/flatly/bootstrap.css';
+import 'toastr/build/toastr.min';
+import 'toastr/build/toastr.css';
+import Navbar from './components/navbar.js';
+//import Rotas from './rotas.js';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className='container'>
+        <Navbar />
+      </div>
+    );
+  }
+}
+
+export default App;
